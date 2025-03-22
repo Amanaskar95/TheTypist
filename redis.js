eb700f244f8e4be53604e52385e92aa22de6216f
Aman Kumar <144769079+Amanaskar95@users.noreply.github.com>
2025-03-22 16:35:35 +0530

    Add files via upload

---
+++ redis.js
@@ -0,0 +1,5 @@
+import Redis from 'ioredis'
+
+const redis = new Redis("rediss://red-cooup9n79t8c73fg00r0:qpNgkmcBcqGSUZ47CwvS9iTzrqBiYKcV@oregon-redis.render.com:6379");
+
+export default redis;
---
+++ server.js
@@ -0,0 +1,79 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import userRoute from "./routes/userRoute.js";
+import testRoute from "./routes/testRoute.js";
+import cookieParser from "cookie-parser";
+import session, { Store, Cookie } from "express-session";
+import MongoStore from "connect-mongo";
+
+const origins = ["http://localhost:5173", "https://thetypist.netlify.app"];
+const auto = true;
+/*configuration*/
+dotenv.config({ path: "./.env" });
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5173","http://localhost:5101", "https://thetypist.netlify.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: "10mb" }));
+app.use(
+  session({
+    secret: "my_secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+    },
+    Cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24,
+      sameSite: "none",
+      secure: true,
+    },
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
+  })
+);
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("<h1>Server is Runing!</h1>");
+});
+app.use("/user", userRoute);
+app.use("/test", testRoute);
+
+/*Mongoose Setup*/
+const PORT = process.env.PORT;
+const connectionSting = process.env.MONGO_URL;
+
+mongoose
+  .connect(connectionSting, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to theTypist MonogDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  .catch((err) => console.error("Error connecting to database:", err));
